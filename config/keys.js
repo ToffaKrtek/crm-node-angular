7875b6a9586a1d2ced5ae65efeccbb7ecdf542ae
ToffaKrtek <71261400+ToffaKrtek@users.noreply.github.com>
2020-10-25 17:27:52 +0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-        mongoURI: 'mongodb+srv://anton:48a15n16t23o42n@fullstack.i7vue.mongodb.net/<dbname>?retryWrites=true&w=majority',
+        mongoURI: 'YOUR_MONGOURI' ,
         jwt: 'dev-jwt',
 }
 
